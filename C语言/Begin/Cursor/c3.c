d5041c1b7b7a23f7390c11f6446ac0f1ececad5c
不会敲代码的阿_rong <1940221438@qq.com>
2025-11-20 16:01:42 +0800

    docs(指针): 添加二级指针解引用和指针数组的文档说明
    添加关于二级指针解引用操作的详细解释和示例代码
    补充指针数组与二级指针关系的说明和示例
    更新代码注释以更清晰地展示指针操作

--- C语言/Begin/Cursor/c3.c
+++ C语言/Begin/Cursor/c3.c
@@ -13,9 +13,19 @@ int main()
 
     int **pp = &p;
 
-    printf("%p\n", p); // 0x7ffdd376b364
-    printf("%p\n", *pp); // 0x7ffdd376b364
+
+    ### 二级指针的解引用（Dereferencing）
+    解引用操作符 * 用于访问指针所指向的值。对于二级指针，你需要使用两次 * 来访问最终的值。
+    - pp ：存储的是 p 的地址。
+    - *pp ：解引用 pp ，得到 pp 所指向的值，也就是 p 的值（ a 的地址）。所以 *pp 等同于 p 。
+    - **pp ：解引用 *pp ，得到 *pp 所指向的值，也就是 a 的值（ 10 ）。所以 **pp 等同于 *p ，也等同于 a 。
+
+    printf("%p\n", p); // 0x7ffdd376b364 --- a的地址
+    printf("%p\n", *pp); // 0x7ffdd376b364 --- a的地址
+
+    printf("%p\n", pp); // 0x7ffdd376b368 --- p的地址
     printf("%d\n", **pp); // 10
+    printf("%d\n", *p); // 10
 
     return 0;
 }
@@ -36,9 +46,11 @@ int main()
 
     for(int i = 0; i < 3; i++)
     {
-        // printf("%d ", *parr[i]); // 10 20 30f
+        // printf("%p ", parr[i]); // 0x7ffd69d12578 0x7ffd69d1257c 0x7ffd69d12580 ---> 指针数组里的地址
+        // printf("%p ", *(pparr + i)); // 0x7ffd69d12578 0x7ffd69d1257c 0x7ffd69d12580 ---> 指针数组里的地址
+        // printf("%d ", *parr[i]); // 10 20 30
         // printf("%d ", **(pparr + i)); // 10 20 30
-        printf("%d ", *pparr[i]); // 10 20 30
+        // printf("%d ", *pparr[i]); // 10 20 30
     }
     printf("\n");
 
