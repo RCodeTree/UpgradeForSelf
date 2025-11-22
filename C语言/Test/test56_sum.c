e401b632c86667bef37ff02c9c481edb7cd45f02
不会敲代码的阿_rong <1940221438@qq.com>
2025-11-22 19:52:30 +0800

    feat: 添加C语言练习题实现
    添加四个C语言练习题实现：
    1. test56_sum.c - 计算数字前n项和
    2. test57_sum.c - 查找水仙花数
    3. test58_sum.c - 汽水瓶兑换问题
    4. test59_graph.c - 打印菱形图案

--- C语言/Test/test56_sum.c
+++ C语言/Test/test56_sum.c
@@ -7,4 +7,30 @@
 
 示例：
 a = 2 → 2 + 22 + 222 + 2222 + 22222
+2 = 0 * 10 + 2
+22 = 2 * 10 + 2
+222 = 22 * 10 + 2
+.........
 */
+#include <stdio.h>
+int main()
+{
+	unsigned int num;
+	unsigned int n = 0;
+	int key = 0;
+	int sum = 0;
+
+	printf("请输入一个整数: ");
+	scanf("%u%u", &num, &n);
+
+	for(int i = 1; i <= n; i++)
+	{
+		key = key * 10 + num;
+
+		sum += key;
+	}
+
+	printf("%u 的前 %u 项之和为: %d\n", num, n, sum);
+
+	return 0;
+}
