bd25167d6c2b2c1da5ef79f6fb67b757255eeb59
不会敲代码的阿_rong <1940221438@qq.com>
2025-11-25 10:20:17 +0800

    feat: 添加汽水兑换计算和菱形打印程序实现
    实现两个C语言程序：
    1. 计算给定金额能兑换的汽水数量，考虑空瓶兑换规则
    2. 根据输入行数打印对称菱形图案，包含上下半部分逻辑

--- C语言/Test/test58_sum.c
+++ C语言/Test/test58_sum.c
@@ -1,3 +1,40 @@
 /*
 喝汽水，1瓶汽水1元，2个空瓶可以换一瓶汽水，给20元，可以多少汽水（编程实现）。
+money ---- 钱
+
+cola ---- 汽水
+
+empty ---- 空瓶
+
+20 money ----> 20 cola, 20 empty
+
+----> 10 cola, 10 empty
+
+----> 5 cola, 5 empty(兑换后余：1 empty)
+
+----> 2 cola, 2 empty + 1 empty（兑换后余：1 empty）
+
+----> 1 cola, 1 empty + 1 empty
+
+----> 1 cola, 1 empty
+
+cola = 20 + 10 + 5 + 2 + 1 + 1 = 39
 */
+#include <stdio.h>
+int main()
+{
+	int money = 20;
+	int cola = money;
+	int empty = money;
+
+	while (empty >= 2)
+	{
+		cola += (empty / 2);
+
+		empty = (empty / 2) + (empty % 2);
+	}
+
+	printf("%d 元能换 %d 个可乐\n", money, cola);
+
+	return 0;
+}
--- C语言/Test/test59_graph.c
+++ C语言/Test/test59_graph.c
@@ -4,4 +4,102 @@
 要求：
 1. 顶部 1 个星号，逐行递增到中间最大宽度，再逐行递减到 1。
 2. 每行星号居中对齐，整体左右对称，形成菱形。
+      *
+     ***
+    *****
+   *******
+  *********
+ ***********
+*************
+ ***********
+  *********
+   *******
+    *****
+     ***
+      *
+
+当 i, j 初始化值为：1
+// 上半部分
+for(int i = 1; i <= line; i++)
+{
+        // 空格
+        for(int j = 1; j <= line - i; j++)
+        {
+                printf(" ");
+        }
+        // *
+        for(int j = 1; j <= 2 * i - 1; j++)
+        {
+                printf("*");
+        }
+        printf("\n");
+}
+
+// 打印下半部分
+for(int i = 1; i <= line - 1; i++)
+{
+        // 空格
+        for(int j = 1; j <= i; j++)
+        {
+                printf(" ");
+        }
+
+        // *
+        for(int j = 1; j <= 2 * line - 2 * i - 1; j++)
+        {
+                printf("*");
+        }
+
+        printf("\n");
+}
+
+
 */
+#include <stdio.h>
+int main()
+{
+	unsigned int line;
+
+	printf("请输入行数: ");
+	scanf("%u", &line);
+
+	// 打印上半部分
+	for(int i = 0; i < line; i++)
+	{
+		// 空格
+		for(int j = 0; j < line - 1 - i; j++)
+		{
+			printf(" ");
+		}
+
+		// *
+		for(int j = 0; j < 2 * i + 1; j++)
+		{
+			printf("*");
+		}
+
+		printf("\n");
+	}
+
+
+
+	// 打印下半部分
+	for(int i = 0; i < line - 1; i++)
+	{
+		// 空格
+		for(int j = 0; j < i + 1; j++)
+		{
+			printf(" ");
+		}
+		
+		// *
+		for(int j = 0; j < 2 * (line - 1) - (1 + i) - i; j++)
+		{
+			printf("*");
+		}
+
+		printf("\n");
+	}
+
+	return 0;
+}	
