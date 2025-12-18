9fb08ff6fc2c283e8bdff9fd816cbf9be7e66cad
不会敲代码的阿_rong <1940221438@qq.com>
2025-12-18 14:05:42 +0800

    refactor(测试): 简化结构体数组的初始化方式

--- C语言/Test/test92_struct.c
+++ C语言/Test/test92_struct.c
@@ -7,17 +7,13 @@ struct Student {
 };
 
 int main() {
-    struct Student arr[5];
     struct Student s1 = {"zhangsan", 1, 94.0f};
     struct Student s2 = {"lisi", 2, 32.2f};
     struct Student s3 = {"wangwu", 3, 41.1f};
     struct Student s4 = {"zhaoliu", 4, 71.3f};
     struct Student s5 = {"qianqi", 5, 98.4f};
-    arr[0] = s1;
-    arr[1] = s2;
-    arr[2] = s3;
-    arr[3] = s4;
-    arr[4] = s5;
+    struct Student arr[5] = {s1, s2, s3, s4, s5};
+
     printf("before sort:\n");
     for (int i = 0; i < 5; i++)
         printf("%s %d %.2f\n", arr[i].name, arr[i].id, arr[i].score);
