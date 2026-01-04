a1ee5ff26dd8ccf289af429556d74575e19a19a8
不会敲代码的阿_rong <1940221438@qq.com>
2026-01-04 22:48:24 +0800

    feat: 添加C语言测试程序test117.c和test118.c

---
+++ C语言/Test/test117.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int fun(int x[], int n)
+{
+    static int sum = 0, i;
+    for(i = 0; i < n; i++) 
+        sum += x[i];
+    return sum;
+}
+
+void main()
+{
+	int a[] = {1, 2, 3, 4, 5}, b[] = {6, 7, 8, 9}, s = 0;
+	s = fun(a, 5 + fun(b, 4)); // a索引越界
+	printf("%d\n", s);
+}
