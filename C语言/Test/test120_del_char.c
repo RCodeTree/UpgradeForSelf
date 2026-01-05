513b4bfcf08032794a5640830ae84b994bdc6455
不会敲代码的阿_rong <1940221438@qq.com>
2026-01-06 00:01:18 +0800

    feat: 添加删除字符串中指定字符的功能

---
+++ C语言/Test/test120_del_char.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void del(char *s, char c) {
+    char *p = s;
+    while (*p != '\0') {
+        if (*p != c) {
+            *s = *p;
+            s++;
+        }
+        p++;
+    }
+    *s = '\0';
+}
+
+int main() {
+    char c;
+    printf("输入要删除的字符: ");
+    scanf(" %c", &c);
+    char str[100];
+    printf("输入字符串: ");
+    scanf("%s", str);
+    del(str, c);
+    printf("删除后的字符串为: %s\n", str);
+    return 0;
+}
