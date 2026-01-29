dc913205f7071198f2ac9944126a209100a1ceb8
不会敲代码的阿_rong <1940221438@qq.com>
2026-01-29 15:07:45 +0800

    refactor(链表): 重构链表创建与合并函数以使用动态内存 - 将静态数组存储改为动态内存分配，避免固定大小限制 - 修复链表合并函数中不必要的临时变量使用 - 启用并完善主函数中的链表合并测试逻辑 - 改进代码格式和变量命名以提高可读性

--- C语言/Test/test107_chain.c
+++ C语言/Test/test107_chain.c
@@ -1,59 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct Node
-{
-	int data;
-	struct Node* next;
-}ns[10];
+struct Node {
+    int data;
+    struct Node *next;
+};
 
-struct Node creat()
-{
-	int i = 0;
-	printf("正在生成单链表，请输入整数，-1就退出> ");
-	while (1)
-	{
-		int n;
-		scanf("%d", &n);
-		if (n == -1) break;
-		ns[i++].data = n;
-	}
-	for (int j = 0; j < i - 1; j++)
-	{
-		ns[j].next = &ns[j + 1];
-	}
-	printf("单链表的值是: \n");
-	printNodeData(&ns[0]);
-	return ns[0];
-		
+struct Node *creat();
+
+struct Node *contracte(struct Node *n1, struct Node *n2);
+
+void printNodeData(struct Node *n);
+
+
+struct Node *creat() {
+    int num_data = 0;
+    printf("正在生成单链表，请输入整数，-1就退出> ");
+    struct Node *head = NULL; // 记录头节点
+    struct Node *tail = NULL; // 记录尾节点，用于连接新节点
+    while (1) {
+        scanf("%d", &num_data);
+        if (num_data == -1) break;
+        struct Node *newNode = (struct Node *) malloc(sizeof(struct Node));
+        newNode->data = num_data;
+        newNode->next = NULL;
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    printf("单链表的值是：\n");
+    printNodeData(head);
+    return head;
 }
 
-struct Node* contracte(struct Node* n1, struct Node* n2)
-{
-	struct Node* t1 = n1;
-	struct Node* t2 = n2;
-	while (t1 -> next != NULL) t1 = t1 -> next;
-	t1 -> next = t2;
-	printNodeData(n1);
-	return n1;
+struct Node *contracte(struct Node *n1, struct Node *n2) {
+    struct Node *t1 = n1;
+    while (t1->next != NULL) t1 = t1->next;
+    t1->next = n2;
+    return n1;
 }
 
 
-void printNodeData(struct Node* n)
-{
-	while (n != NULL) 
-	{
-        	printf("%d\n", n->data);
-        	n = n->next;
-    	}
+void printNodeData(struct Node *n) {
+    while (n != NULL) {
+        printf("%d\n", n->data);
+        n = n->next;
+    }
 }
 
-int main()
-{
-	struct Node n1 = creat();
-	struct Node n2 = creat();
-//	struct Node* concatLink = contracte(&n1, &n2);
-//	printf("合并后的单链表结果为: ");
-//	printNodeData(concatLink);
-	return 0;
+int main() {
+    struct Node *n1 = creat();
+    struct Node *n2 = creat();
+    struct Node *concatLink = contracte(n1, n2);
+    printf("合并后的单链表的值是：\n");
+    printNodeData(concatLink);
+    return 0;
 }
-	
