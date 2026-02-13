56bc6083fa3ff7dc93122a62760a55087933344b
不会敲代码的阿_rong <1940221438@qq.com>
2026-02-13 16:43:59 +0800

    feat: 添加杨辉三角形C语言实现
    - 实现杨辉三角形的二维数组计算逻辑
    - 添加正向三角形输出功能
    - 添加反向三角形输出功能
    - 使用制表符实现三角形格式化显示
    - 包含5行杨辉三角形的完整计算和展示
    .[3~[3~

---
+++ C语言/Test/test143_yhsj.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[5][5] = {0};
+	// 杨辉三角
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			if (i == 0) a[i][j] = 1;
+			if (j == 0) a[i][j] = 1;
+			if (i == j) a[i][j] = 1;
+			if (i > 1 && j >= 1) a[i][j] = a[i - 1][j - 1] + a[i - 1][j];
+		}
+	}
+
+	// 正三角
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5 - (i + 1); j++)
+		{
+			printf("\t");
+		}
+		for (int k = 0; k <= i; k++)
+		{
+			printf("\t%d\t", a[i][k]);
+		}
+		printf("\n");
+	}
+
+	// 倒三角
+	for (int i = 4; i >= 0; i--)
+	{
+		for (int j = 0; j < 5 - (i + 1); j++)
+		{
+			printf("\t");
+		}
+		for (int k = 0; k <= i; k++)
+		{
+			printf("\t%d\t", a[i][k]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
