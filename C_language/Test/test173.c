cb4f278a9397407a176a988ae740b030aeafa5f2
不会敲代码的阿_rong <1940221438@qq.com>
2026-02-27 18:24:34 +0800

    refactor(C_language): 重构质数判断和数组操作代码
    - 优化质因子分解算法，使用while循环替代if语句进行因式分解
    - 添加详细的函数注释说明参数和返回值含义
    - 移除过时的注释代码块和改进版本的质数判断函数
    - 新增test173.c文件实现多个数组操作功能
    - 实现数组元素整除判断、两数组交集、并集及字符串拷贝等功能
    - 添加字符数组操作和字母转换的多种编程练习题解

---
+++ C_language/Test/test173.c
@@ -0,0 +1,169 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define N 6
+
+
+int main()
+{
+    // 6  输入20个整数存入一数组，输出其中能被数组中其它元素(至少有一个)整除的那些数组元素
+    // int a[N] = {2, 4, 6, 8, 10, 12};
+    // for (int i = 0; i < N; i++)
+    // {
+    //     int flag = 0;
+    //     for (int j = 0; j < N; j++)
+    //     {
+    //         if (j != i)
+    //         {
+    //             if (a[i] % a[j] == 0)
+    //             {
+    //                 flag = 1;
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     if (flag) printf("%d ", a[i]);
+    // }
+    // printf("\n");
+    int a[N] = {2, 4, 6, 8, 10, 12};
+    for (int i = 0; i < N; i++)
+    {
+        int flag = 0; // 标记是否存在其他元素能整除a[i]
+        for (int j = 0; j < N; j++)
+        {
+            if (j != i && a[i] % a[j] == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag)
+        {
+            printf("%d ", a[i]);
+        }
+    }
+    printf("\n");
+
+
+    // 7  输入两个数组（数组元素个数自定），输出在两个数组中都出现的元素（如a[5]={2,3,4,5,6}，b[6]={3,5,7,9,10,-1}，则输出3、5）
+    // int a[5], b[5], c[1000] = {0};
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     scanf("%d", &a[i]);
+    // }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     scanf("%d", &b[i]);
+    // }
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     for (int j = 0; j < 5; j++)
+    //     {
+    //         if (a[i] == b[j])
+    //         {
+    //             c[a[i]] = a[i];
+    //         }
+    //     }
+    // }
+    // for (int i = 0; i < 1000; i++)
+    // {
+    //     if (c[i] != 0) printf("%d ", c[i]);
+    // }
+    // printf("\n");
+    // 更高效的交集查找（不需要额外数组）
+    // printf("交集元素：");
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     int isDuplicate = 0;
+    //     // 检查当前元素是否已经输出过
+    //     for (int j = 0; j < i; j++)
+    //     {
+    //         if (a[i] == a[j])
+    //         {
+    //             isDuplicate = 1;
+    //             break;
+    //         }
+    //     }
+    //
+    //     if (!isDuplicate)
+    //     {
+    //         // 检查是否在b数组中
+    //         for (int k = 0; k < 5; k++)
+    //         {
+    //             if (a[i] == b[k])
+    //             {
+    //                 printf("%d ", a[i]);
+    //                 break;
+    //             }
+    //         }
+    //     }
+    // }
+    // printf("\n");
+
+    // 8  输入两个数组（数组元素个数自定），输出在两个数组中都不出现的元素（如a[5]={2,3,4,5,6}，b[6]={3,5,7,9,10,-1}，则输出2、4、6、7、9、10、-1）
+    /*int a[5] = {2, 3, 4, 5, 6}, b[6] = {3, 5, 7, 9, 10, -1};
+    for (int i = 0; i < 5; i++)
+    {
+        int flag = 0;
+        for (int j = 0; j < 6; j++)
+        {
+            if (a[i] == b[j])
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (!flag) printf("%d ", a[i]);
+    }
+    for (int i = 0; i < 6; i++)
+    {
+        int flag = 0;
+        for (int j = 0; j < 5; j++)
+            if (b[i] == a[j])
+            {
+                flag = 1;
+                break;
+            }
+        if (!flag) printf("%d ", b[i]);
+    }*/
+
+
+    // 9  编程，将字符数组S2中的全部字符拷贝到字符数组S1中（不用strcpy函数）
+    /*char S1[100], S2[100];
+    puts("输入S1：");
+    gets(S1);
+    puts("输入S2：");
+    gets(S2);
+    printf("复制前S1 = %s\n", S1);
+    int i = 0;
+    while (S2[i] != '\0')
+    {
+        S1[i] = S2[i];
+        i++;
+    }
+    S1[i] = '\0';
+    printf("复制后S1 = %s\n", S1);*/
+
+    // 11  输入一行小写字母后，将字母变成其下一字母（a变成b、b变成c、c变成d、…、x变成y、y变成z、z变成a）输出
+    /*char* s = malloc(100);
+    printf("请输入小写字母：");
+    scanf("%s", s);
+    char* p = s;
+    while (*p)
+    {
+        if (*p >= 'a' && *p <= 'y')
+        {
+            *p = *p + 1;
+        }
+        else if (*p == 'z')
+        {
+            *p = 'a';
+        }
+        p++;
+    }
+    printf("%s\n", s);
+    free(s);*/
+    return
+        0;
+}
