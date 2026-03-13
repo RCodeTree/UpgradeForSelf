6c44393cbd474bc807e778711ff2beeb5ae04f49
不会敲代码的阿_rong <1940221438@qq.com>
2026-03-13 23:51:13 +0800

    fix(Test): 修正函数参数中字符数组的维度声明
    修复 fun 函数中字符数组参数维度声明错误，从 [3] 改为 [4] 以匹配实际使用
    移除有语法错误的注释代码

--- C_language/Test/test193_char_arr.c
+++ C_language/Test/test193_char_arr.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-char* fun(char (*a)[3], int num, char* max)
+char* fun(char (*a)[4], int num, char* max)
 {
     int i = 0;
     max = a[0];
@@ -16,6 +16,7 @@ int main()
     char a[3][4] = {{'a', 'b', 'c'}, {'d', 'e', 'f'}, {'g', 'h', 'i'}};
     char b[3][4] = {"abc", "def", "ghi"};
     char* c[3] = {"a31242fabc", "dfasdfef", "ghidafa"};
+    // char* f[3] = {{'a', 'b', 'c'}, {'d', 'e', 'f', 'a', 's', 'd', 'f', 'e', 'f'}, {'g', 'h', 'i', 'd', 'a', 'f', 'a'}}; // - 问题 ： 语法错误 - 原因 ： {'a', 'b', 'c'} 是字符数组初始化器，不能用于初始化 char* 类型的元素
 
     char d[] = "1234567890";
     char e[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
