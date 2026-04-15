bb5dc16801c9705100bdc960948c7bd899a1a8e4
不会敲代码的阿_rong <1940221438@qq.com>
2026-04-15 20:31:56 +0800

    feat: 添加求交集功能并重命名并集函数
    重命名并集函数为 unionSet 以提高可读性
    添加 intersectionSet 函数实现求交集功能
    更新主函数以测试新功能.

--- C_language/Test2/test23.c
+++ C_language/Test2/test23.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // 求并集
-int f(int* a, int* b, int* c, int la, int lb, int lc)
+int unionSet(int* a, int* b, int* c, int la, int lb, int lc)
 {
     int cl = 0;
     for (int i = 0; i < la; i++)
@@ -42,14 +42,56 @@ int f(int* a, int* b, int* c, int la, int lb, int lc)
     return cl;
 }
 
+// 求交集
+int intersectionSet(int* a, int* b, int la, int lb, int* c, int lc)
+{
+    int i, j, k;
+    int q = 0;
+    for (i = 0; i < la; i++)
+    {
+        int t = 0;
+        for (j = 0; j < lb; j++)
+        {
+            if (a[i] == b[j])
+            {
+                t = b[j];
+                break;
+            }
+        }
+        int flag = 0;
+        for (k = 0; k < lc; k++)
+        {
+            if (t == c[k])
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0) c[q++] = t;
+    }
+    for (int i = 0; i < q; i++)
+        for (int j = 0; j < q - 1 - i; j++)
+            if (c[j] > c[j + 1])
+            {
+                int temp = c[j];
+                c[j] = c[j + 1];
+                c[j + 1] = temp;
+            }
+    return q;
+}
+
 int main()
 {
     int
         a[5] = {2, 3, 13, 4, 3},
         b[6] = {2, 3, 11, 9, 1, 0},
-        c[100] = {0};
-    int len = f(a, b, c, 5, 6, 100);
+        c[100] = {0},
+        d[100] = {0};
+    int len = unionSet(a, b, c, 5, 6, 100);
     for (int i = 0; i < len; i++) printf("%d ", c[i]);
+    printf("\n");
+    len = intersectionSet(a, b, 5, 6, d, 100);
+    for (int i = 0; i < len; i++) printf("%d ", d[i]);
     printf("\n");
     return 0;
 }
