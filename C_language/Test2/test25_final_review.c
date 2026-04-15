70c91470cb353de1793aeb222112a9fde23a708e
不会敲代码的阿_rong <1940221438@qq.com>
2026-04-15 20:02:32 +0800

    refactor(test25_final_review.c): 优化代码格式并添加新功能
    - 调整循环和条件语句的缩进格式以提高可读性
    - 为函数添加文档注释以说明其功能
    - 新增数组游标计算、公共字符移除和求和功能
    - 添加测试用例以验证新增功能

--- C_language/Test2/test25_final_review.c
+++ C_language/Test2/test25_final_review.c
@@ -34,11 +34,12 @@ void insertString2(char* a, char* b)
     int lb = strlen(b);
     char maxc = a[0];
     int maxi = 0;
-    for (int i = 1; i < la; i++) if (a[i] > maxc)
-    {
-        maxc = a[i];
-        maxi = i;
-    }
+    for (int i = 1; i < la; i++)
+        if (a[i] > maxc)
+        {
+            maxc = a[i];
+            maxi = i;
+        }
     for (int i = la; i >= maxi + 1; i--) a[i + lb] = a[i];
     int j = 0;
     for (int i = maxi + 1; b[j] != '\0'; i++, j++) a[i] = b[j];
@@ -66,6 +67,7 @@ void connectString(char* a, char* b)
 }
 
 /**
+ * 计算x和y的大小关系
  * @param x 整数x
  * @param y 整数y
  * @return 整数x
@@ -78,6 +80,7 @@ int num(int x, int y) // x = 1, y = 0
 }
 
 /**
+ * 打印x和y的大小关系
  * @param x 整数x
  * @param y 整数y
  * @return 无
@@ -93,19 +96,71 @@ void printNum(int x, int y)
     printf("%d\n", x++);
 }
 
+/**
+ * 将8进制字符串转换为十进制整数
+ * @param s 字符串s
+ * @return 整数r
+ * @note 无
+ */
 double eightToTen(char* s)
 {
     int l = strlen(s);
-	int n = l - 1;
-	double r = 0;
-	while (l)
-	{
-		r += pow(8, n) * (*s - '0');
-		n--;
-		s++;
-		l--; 
-	}
-	return r;
+    int n = l - 1;
+    double r = 0;
+    while (l)
+    {
+        r += pow(8, n) * (*s - '0');
+        n--;
+        s++;
+        l--;
+    }
+    return r;
+}
+
+/**
+ * 计算数组arr的游标位置
+ * @param arr 数组arr
+ * @return 整数*(p+2) - *(--p)
+ * @note 无
+ */
+int arrCursor(int* arr)
+{
+    int* p = arr;
+    return *(p + 2) - *(--p);
+}
+
+/**
+ * 移除字符串s中的公共字符
+ * @param s 字符串s
+ * @return 整数1
+ * @note 无
+ */
+int removeCommonChar(char* s)
+{
+    int i, j;
+    for (i = 0; i < strlen(s); i++)
+        for (j = i + 1; j < strlen(s); j++)
+            if (s[i] == s[j])
+            {
+                // strcpy(s+j,s+j+1);
+                int k = j;
+                for (; s[k] != '\0'; k++) s[k] = s[k + 1];
+                return 1;
+            }
+    return 0;
+}
+
+/**
+ * 计算1到m的和
+ * @param m 整数m
+ * @return s
+ */
+int getSum(int m)
+{
+    static int i = 2;
+    int s = 0;
+    for (; i <= m; i++) s += i;
+    return s;
 }
 
 int main()
@@ -141,8 +196,33 @@ int main()
 
     printf("----------------------------------\n");
 
-	printf("%.0f\n", eightToTen("556"));
-    
-	printf("----------------------------------\n");
+    printf("%.0f\n", eightToTen("556"));
+
+    printf("----------------------------------\n");
+
+    int arr[5] = {2, 4, 6, 8, 10};
+    int* p = arr + 1;
+    printf("%d\n", arrCursor(p));
+
+    printf("----------------------------------\n");
+
+    char x[] = "Office--2007";
+    puts(x);
+    while (removeCommonChar(x)) puts(x);
+
+    printf("----------------------------------\n");
+
+    printf("%d\n", getSum(3) + getSum(5));
+
+    printf("----------------------------------\n");
+
+    int a1[4][4] = {{1, 2, 3, 4}, {2, 3, 4, 5}, {3, 4, 5, 6}, {4, 5, 6, 7}}, i, j;
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 1; j <= 3 - i; j++) printf("   ");
+        for (j = 3 - i; j < 4; j++) printf("%3d", a1[i][j]);
+        putchar('\n');
+    }
+
     return 0;
 }
